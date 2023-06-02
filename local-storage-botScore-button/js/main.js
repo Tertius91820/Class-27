5452fc9a69b6c64126786ddd8fccc013c356c60b
tertius Jubber <tjubber@gmail.com>
2023-06-02 10:07:18 +0200

    "Card Game added"

--- local-storage-botScore-button/js/main.js
+++ local-storage-botScore-button/js/main.js
@@ -1,3 +1,13 @@
-//Create a button that adds 1 to a botScore stored in localStorage 
+if( !localStorage.getItem("botScore") ){
+  localStorage.setItem("botScore",0)
+}
 
 
+document.querySelector('button').addEventListener('click', addOne)
+
+function addOne(){
+  let botScoreVal = Number(localStorage.getItem("botScore"))
+  botScoreVal += 1
+  localStorage.setItem("botScore",botScoreVal)
+  document.querySelector("h3").innerText = botScoreVal
+}
