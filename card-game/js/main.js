73d62b97813ccd6a316ee1de1786dae4532a0b45
tertius Jubber <tjubber@gmail.com>
2023-06-04 11:16:01 +0200

    "Some styling applied"

--- card-game/js/main.js
+++ card-game/js/main.js
@@ -1,27 +1,56 @@
-//Example fetch using pokemonapi.co
-document.querySelector('button').addEventListener('click', getFetch)
+let deckId = ""
 
-function getFetch(){
-  const choice = document.querySelector('input').value.toLowerCase()
-  const url = `https://api.nasa.gov/planetary/apod?api_key=PnOZ7hfYOKiPnk6eEZX8nEap8AKMilXMTH3Vzq0j&date=${choice}`
+  let url = `https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`
   
-
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
         console.log(data)
-        if(data.media_type === "image"){
-          document.querySelector("img").src = data.hdurl 
-        }else if(data.media_type ==="video"){
-          document.querySelector('iframe').src = data.url
-        }
-
-        document.querySelector("h3").innerText = data.explanation
-        
+        deckId = data.deck_id
 
       })
       .catch(err => {
           console.log(`error ${err}`)
       });
-}
 
+
+    document.querySelector("button").addEventListener("click",drawTwo)
+
+    function drawTwo(){
+      const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
+      
+
+      fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        document.querySelector("#player1").src = data.cards[0].image
+        document.querySelector("#player2").src = data.cards[1].image
+        let player1Val = convertToNUm(data.cards[0].value)
+        let player2Val = convertToNUm(data.cards[1].value)
+        if(player1Val > player2Val){
+          document.querySelector("h3").innerText = "Player 1 Wins!"
+        }else if(player1Val < player2Val){
+          document.querySelector("h3").innerText = "Player 2 Wins!"
+        }else{
+          document.querySelector("h3").innerText = "Time For War!!!"
+        }
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+    }
+
+  function convertToNUm(val){
+    if(val ==="ACE"){
+      return 14
+    }else if(val === "KING"){
+      return 13
+    }else if(val === "QUEEN"){
+      return 12
+    }else if(val === "JACK"){
+      return 11
+    }else{
+      return Number(val)
+    }
+  }
